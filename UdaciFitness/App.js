34bca7bdb14b640057ad0fe0dd6f61159d397820
Daniel Escobar <escobard90@gmail.com>
2017-09-21 20:06:39 -0600

    doc: updated commentary for react native base components

--- UdaciFitness/App.js
+++ UdaciFitness/App.js
@@ -2,6 +2,16 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  componentDidMount(){
+    console.log('Before')
+    debugger
+    console.log('After')
+    // testing comment
+    // comments work outside of the `render` method due to how react native compiles code for each native OS
+    // Two common elements with native are:
+    // <View> Similar to a div, you can attach styles and include nested components within
+    // <Text> Same as a p or span tag
+  }
   render() {
     return (
       <View style={styles.container}>
