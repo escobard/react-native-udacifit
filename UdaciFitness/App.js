897e0cd3d84d31c7d59b44eff448ccf2c6d5833d
Daniel Escobar <escobard90@gmail.com>
2017-09-21 20:19:33 -0600

    feat: added icons to my application via @expo/vector-icons

--- UdaciFitness/App.js
+++ UdaciFitness/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// we can use the following to import icons with the following code
+// the import path is provided by the icon library we choose
+import { Ionicons } from '@expo/vector-icons'
 
 export default class App extends React.Component {
   componentDidMount(){
@@ -10,11 +13,17 @@ export default class App extends React.Component {
     // comments work outside of the `render` method due to how react native compiles code for each native OS
     // Two common elements with native are:
     // <View> Similar to a div, you can attach styles and include nested components within
+    // notice how the styles are added to the View below - these are created as a const and then added to the native app accordingly
     // <Text> Same as a p or span tag
+    // for icons with the react-native-app starter library it has support for vector icons that can be seen here: https://expo.github.io/vector-icons/
+    // another list can be found here: https://github.com/oblador/react-native-vector-icons
+    // Add icons by utilizing the imported component as shown below
+    // can add options such as color for color and size for size
   }
   render() {
     return (
       <View style={styles.container}>
+        <Ionicons name='ios-pizza' color='red' size={200}/>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
