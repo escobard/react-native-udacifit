3ecb924e904bf051d8b79a5b967566044fb98c29
Daniel Escobar <escobard90@gmail.com>
2017-09-26 23:57:05 -0600

    feat: added UdaciStepper component

--- UdaciFitness/components/UdaciSlider.js
+++ UdaciFitness/components/UdaciSlider.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { View, Text, Slider } from 'react-native'
 
+
+// the units here are from the `helper.js` file which are passed into this component with the {...rest} operand which passes the
+// remaining properties from helper.js into this component as props - very neat!
 export default function UdaciSlider({max, unit, step, value, onChange}){
 	return (
 		<View>
--- UdaciFitness/components/UdaciStepper.js
+++ UdaciFitness/components/UdaciStepper.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { FontAwesome, Entypo } from '@expo/vector-icons'
 
-export default function UdaciStepper(){
+// again, passed from the properties in helper.js into the component with the {...rest} method
+export default function UdaciStepper({max, unit, step, value, onIncrement, onDecrement}){
 	return (
 		<View>
-
-			<Text>UdaciStepper</Text>
-
+			<TouchableOpacity onPress={onDecrement}>
+				<FontAwesome name='minus' size={30} color={'black'} />				
+			</TouchableOpacity>
+			<TouchableOpacity onPress={onIncrement}>
+				<FontAwesome name='plus' size={30} color={'black'} />				
+			</TouchableOpacity>
+			<View>
+				<Text>{value}</Text>
+				<Text>{unit}</Text>
+			</View>
 		</View>
 	)
 }
