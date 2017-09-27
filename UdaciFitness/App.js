4f57f4d73d92d2833f94e303429410b86f734109
Daniel Escobar <escobard90@gmail.com>
2017-09-26 23:13:44 -0600

    feat: slider basics

--- UdaciFitness/App.js
+++ UdaciFitness/App.js
@@ -3,65 +3,24 @@ import {
   StyleSheet,
   Text, 
   View,
-
-  // This transforms the component from one color to another when you press on it
-  TouchableHighlight,
-  // only usable in android, and gives the button the ripple effect seen in native android apps
-  TouchableNativeFeedback,
-  // changes the opacity of the button so that the background is see through
-  TouchableOpacity,
-  // does not change the button apperance and just returns the function
-  TouchableWithoutFeedback 
+  Slider
 } from 'react-native';
 
-// we can use the following to import icons with the following code
-// the import path is provided by the icon library we choose
-import { Ionicons } from '@expo/vector-icons'
-
-import AddEntry from './components/AddEntry';
 
 export default class App extends React.Component {
-  componentDidMount(){
-    console.log('Before')
-    debugger
-    console.log('After')
-    // testing comment
-    // comments work outside of the `render` method due to how react native compiles code for each native OS
-    // Two common elements with native are:
-    // <View> Similar to a div, you can attach styles and include nested components within
-    // notice how the styles are added to the View below - these are created as a const and then added to the native app accordingly
-    // <Text> Same as a p or span tag
-    // for icons with the react-native-app starter library it has support for vector icons that can be seen here: https://expo.github.io/vector-icons/
-    // another list can be found here: https://github.com/oblador/react-native-vector-icons
-    // Add icons by utilizing the imported component as shown below
-    // can add options such as color for color and size for size
-  }
-  handlePress(){
-    alert('Hello!');
+  state ={
+    value: 0
   }
   render() {
     return (
       <View style={styles.container}>
-        <Ionicons name='ios-pizza' color='red' size={200}/>
-        <AddEntry />
-        <TouchableHighlight style={styles.btn} onPress={this.handlePress} underlayColor='#000'>
-          <Text style={styles.btnText}>Touchable Highlight</Text>
-        </TouchableHighlight>
-        <TouchableOpacity style={styles.btn} onPress={this.handlePress}>
-          <Text style={styles.btnText}>Touchable Opacity</Text>
-        </TouchableOpacity>
-        <TouchableWithoutFeedback onPress={this.handlePress}>
-            <View style={styles.btn}>
-              <Text style={styles.btnText}>Touchable Opacity</Text>
-            </View>
-        </TouchableWithoutFeedback>
-        <TouchableNativeFeedback 
-         background={TouchableNativeFeedback.SelectableBackground()}
-         onPress={this.handlePress}>
-            <View style={styles.btn}>
-              <Text style={styles.btnText}>Touchable Opacity</Text>
-            </View>
-        </TouchableNativeFeedback>
+        <Slider
+         value={this.state.value}
+         onValueChange={(value) => this.setState(()=>({value}))}
+         minimumValue={-10}
+         maximumValue={10}
+         step={1} />
+        <Text>Value: {this.state.value}</Text>
       </View>
     );
   }
@@ -72,7 +31,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    marginLeft:10,
+    marginRight: 10,
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
   btn: {
