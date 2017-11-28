c0a07e643a5840ef4e04405c84affcdc55367bdf
Daniel Escobar <escobard90@gmail.com>
2017-11-27 20:41:05 -0700

    style: added base styles to icons and App.js

--- UdaciFitness/App.js
+++ UdaciFitness/App.js
@@ -12,9 +12,6 @@ import reducer from './reducers'
 import AddEntry from './components/AddEntry';
 
 export default class App extends React.Component {
-  state ={
-    value: 0
-  }
   render() {
     return (
       <Provider store={createStore(reducer)}>
@@ -30,22 +27,5 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    marginLeft:10,
-    marginRight: 10,
-    alignItems: 'stretch',
-    justifyContent: 'center',
-  },
-  btn: {
-    backgroundColor: '#E53224',
-    padding: 10,
-    paddingLeft: 50,
-    paddingRight: 50,
-    justifyContent : 'center',
-    alignItems: 'center',
-    borderRadius: 5
-  },
-  btnText: {
-    color: '#fff'
   }
 });
