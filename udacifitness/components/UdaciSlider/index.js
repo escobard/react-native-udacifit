e963d70489c5665c5b99e0e9245184c69e79615c
Daniel Escobar <escobard90@gmail.com>
2017-11-29 15:11:56 -0700

    refactor: fragmented UdaciSlider

--- udacifitness/components/UdaciSlider/index.js
+++ udacifitness/components/UdaciSlider/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import { View, Text, Slider } from 'react-native'
 
+import { styles } from './styles'
 
-// the units here are from the `helper.js` file which are passed into this component with the {...rest} operand which passes the
-// remaining properties from helper.js into this component as props - very neat!
+// the units here are from the `helper.js` file which are passed into this component
+// with the {...rest} operand which passes the remaining properties from helper.js 
+// into this component as props - very neat!
 export default function UdaciSlider({max, unit, step, value, onChange}){
 	return (
 		<View>
---
+++ udacifitness/components/UdaciSlider/styles.js
@@ -0,0 +1,7 @@
+import { StyleSheet } from 'react-native'
+
+import { purple } from '../../utils/colors'
+
+export const styles = StyleSheet.create({
+	
+})
