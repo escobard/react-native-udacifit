248c5d5747da6672e6308fec4a63d4bb7c72660a
Daniel Escobar <escobard90@gmail.com>
2018-01-08 19:56:50 -0700

    feat: added notification util

---
+++ udacifitness/utils/Notifications/constants.js
@@ -0,0 +1,15 @@
+export const NOTIFICATION_KEY = 'UdaciFitness:notifications'
+
+export const NOTIFICATION = {
+		title: 'Log your stats!'.
+		body:"don't forget to log your stats for today!",
+		ios: {
+			sound: true,
+		},
+		android: {
+			sound:true,
+			priority: 'high',
+			sticky: false,
+			vibrate: true,
+		}
+}
---
+++ udacifitness/utils/Notifications/index.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { AsyncStorage } from 'react-native'
+import { Notifications, Permission } from 'expo'
+
+import { NOTIFICATION_KEY, NOTIFICATION } from './constants'
+
+export function clearLocalNotification(){
+	return AsyncStorage.removeItem(NOTIFICATION_KEY)
+		.then(Notifications.cancelAllScheduledNotificationsAsync())
+}
+
+// returns an object which represents the notification
+export function createNotification(){
+	return NOTIFICATION
+}
+
+// checks if the user has already been presented with a notification
+export function setLocalNotification(){
+	
+	// checks if notification key has been logged into AsyncStorage
+	AsyncStorage.getItem(NOTIFICATION_KEY)
+	.then(JSON.parse)
+	.then((data) =>{
+
+		//handles cases where the user is receiving notifications for the first time
+		if (data === null) {
+			Permissions.asAsync(Permissions.NOTIFICATIONS)
+
+			// checks the returned value of the permission above
+			.then(({ status }) =>{
+
+				// if a notification has been granted, schedule another for tomorrow
+				if (status === 'granted') {
+					Notifications.cancelAllScheduledNotificationsAsync()
+				
+
+					// sets the date for the new notification
+					let tomorrow = new Date()
+					tomorrow.setDate(tomorrow.getDate() + 1)
+					tomorrow.setHours(20)
+					tomorrow.setMinutes(0)
+
+					// creates the notification with the object returned from the function above
+					Notifications.scheduleLocalNotificationsAsync(
+						createNotification(),
+						{
+							time: tomorrow,
+							repeat: 'day',
+
+						}
+					)
+				
+				AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true))
+				}
+
+			})
+		}
+	})
+}
